c33abfa60708457eabd328336cb8ac11fbd497c5
drDeepl <Vlimaster@yandex.ru>
2023-09-21 22:55:59 +0700

    added toolbar

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lab1
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val meowButton: Button = findViewById()
+    }
+}
