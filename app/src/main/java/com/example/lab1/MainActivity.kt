aba1c87e4ba9c45b770fd4f96ffc68fe8ddef618
drDeepl <Vlimaster@yandex.ru>
2023-09-24 19:05:25 +0700

    added dice

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,12 +2,48 @@ package com.example.lab1
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle){
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val meowButton: Button = findViewById()
+        val nameButton: Button = findViewById(R.id.btn_name)
+        nameButton.setOnClickListener{
+            toChangeText("01.03.02")
+
+        }
+
+        val diceBtn: Button = findViewById(R.id.btnDice)
+        diceBtn.setOnClickListener {
+            rollDice()
+        }
     }
+
+    private fun toChangeText(text: String){
+        var textCat: TextView = findViewById(R.id.textCat)
+        textCat.text = text
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun rollDice() {
+        val random: Random = Random();
+        val randomInt: Int = random.nextInt(6)+1;
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+        diceImage.setImageResource(drawableResource)
+    }
+
 }
