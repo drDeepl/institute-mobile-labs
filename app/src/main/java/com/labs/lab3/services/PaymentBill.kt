4d370b41d8375bdd58004879810c167a37ffa24f
drDeepl <Vlimaster@yandex.ru>
2023-10-06 00:10:14 +0700

    added UtitlityBillsActivity

---
+++ app/src/main/java/com/labs/lab3/services/PaymentBill.kt
@@ -0,0 +1,10 @@
+package com.labs.lab3.services
+
+class PaymentBill {
+    private val warmBill: Double = 0.0
+    private val waterBill: Double = 0.0
+    private val electricBill: Double = 0.0
+    private val warmWaterBill: Double = 0.0
+    private val coldWaterBill: Double = 0.0
+    private val tkoBill: Double = 0.0
+}
